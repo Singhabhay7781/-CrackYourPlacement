045ed6deca18974735cb20c864d417e971590d26
Abhay Singh <abhaysingh7755as@gmail.com>
2024-08-19 22:58:49 +0530

    19 August

---
+++ Tree/PathSum.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        
+        if (root.left == null && root.right == null && root.val == targetSum) {
+            return true;
+        }
+        int newTargetSum = targetSum - root.val;
+        return hasPathSum(root.left, newTargetSum) || hasPathSum(root.right, newTargetSum);
+    }
+}
