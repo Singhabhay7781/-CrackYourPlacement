2c4a22b4e548e7b77c3c97b1cba4f9be74a7a15c
Abhay Singh <abhaysingh7755as@gmail.com>
2024-09-04 17:40:48 +0530

    4 September

---
+++ Array/FindDuplicates.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+
+        for(int i=0;i<nums.length;i++){
+            int val = Math.abs(nums[i]);
+
+            if(nums[val - 1] < 0){
+                res.add(val);
+            } else {
+                nums[val - 1] = -nums[val-1];
+            }
+        }
+        return res;
+    }
+}
