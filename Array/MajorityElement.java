29a49801cc40164c077d2a02442c479f7e4572cd
Abhay Singh <abhaysingh7755as@gmail.com>
2024-09-06 22:27:30 +0530

    6 September

---
+++ Array/MajorityElement.java
@@ -0,0 +1,21 @@
+class Solution {
+   public int majorityElement(int[] nums) {
+    int majorityCandidate = nums[0];
+    int count = 1;
+
+    for (int i = 1; i < nums.length; i++) {
+        if (nums[i] == majorityCandidate) {
+            count++;
+        } else {
+            count--;
+            if (count == 0) {
+                majorityCandidate = nums[i];
+                count = 1;
+            }
+        }
+    }
+
+    return majorityCandidate;
+}
+
+}
